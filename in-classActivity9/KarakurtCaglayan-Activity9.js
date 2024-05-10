d1681884a83c2383ea202027917ea9342a9098c4
Osmancan Çağlayan <134306439+Caglyn@users.noreply.github.com>
2024-05-10 15:08:16 +0300

    Add files via upload

---
+++ in-classActivity9/KarakurtCaglayan-Activity9.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    // preload images
+    $("#image_list a").each(function() {
+        var swappedImage = new Image();
+        swappedImage.src = $(this).attr("href");
+    });
+
+    // set up event handlers for links    
+    $("#image_list a").click(function(evt) {
+
+        var imageURL = $(this).attr("href");
+        var caption = $(this).attr("title");
+
+        $("#image, #caption").fadeOut("slow", function() {
+            $(this).attr("src", imageURL);
+            $("#caption").text(caption);
+            $("#image, #caption").fadeIn("slow");
+        });
+
+		// cancel the default action of the link
+        evt.preventDefault();
+    }); // end click
+
+    // move focus to first thumbnail
+    $("li:first-child a").focus();
+}); // end ready
